0c3c204e4fd3b61e3981fc739b7fd7bfa82e4c16
Ubuntu <ubuntu@ip-172-31-51-164.us-west-2.compute.internal>
2023-02-12 18:19:27 +0000

    initial commit

--- nautobot_bgp_models/__init__.py
+++ nautobot_bgp_models/__init__.py
@@ -23,8 +23,8 @@ class NautobotBGPModelsConfig(PluginConfig):
     description = "Nautobot BGP Models Plugin."
     base_url = "bgp"
     required_settings = []
-    min_version = "1.5.4"
-    max_version = "1.999"
+    min_version = "2.0.0a1"
+    max_version = "2.999"
     default_settings = {
         "default_statuses": {
             "AutonomousSystem": ["active", "available", "planned"],
